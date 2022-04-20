2410591a08eef9cc9edd222707c0062b31b2d320
Adam Strickland <adam.strickland@gmail.com>
2022-04-20 17:55:31 -0400

    fix issues

--- spec/brightline_spec.rb
+++ spec/brightline_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Brightline do
   it "has a version number" do
     expect(Brightline::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
