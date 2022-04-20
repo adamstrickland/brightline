ec3fb525bbad8b0d752a09a9e2267066b73d5801
Adam Strickland <adam.strickland@gmail.com>
2022-04-20 15:01:26 -0400

    initial

---
+++ spec/brightline_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Brightline do
+  it "has a version number" do
+    expect(Brightline::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
