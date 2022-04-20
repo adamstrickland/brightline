e23093d9ff83b3ba1b2d1552deaabed0796f4909
Adam Strickland <adam.strickland@gmail.com>
2022-04-20 17:55:31 -0400

    fix issues

--- lib/brightline/fact_consumer.rb
+++ lib/brightline/fact_consumer.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require "active_support/concern"
 
-module Brightline::FactConsumer
-  extend ActiveSupport::Concern
-
+module Brightline
+  module FactConsumer
+    extend ActiveSupport::Concern
+  end
 end
